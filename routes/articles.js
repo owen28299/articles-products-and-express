9bbe464a6326942e401b8ed34b92f1e760ae7a60
Owen <owenyangg@gmail.com>
2016-04-13 15:00:19 -1000

    basic post for articles setup

--- routes/articles.js
+++ routes/articles.js
@@ -7,6 +7,9 @@ router.route('/')
   .get ((req, res) => {
     res.send('Hello World!');
     console.log(req.body);
+  })
+  .post( (req,res) => {
+    res.send("posted article");
   });
 
 module.exports = router;
